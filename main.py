44fad4f4f1149a9176999a2835fb4d48fd36a495
Fensoa <hugo.andriamaromanana@laplateforme.io>
2023-01-20 13:12:26 +0100

    [ADD] play again, check__if_game_over func

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ content = content.split('\n')
 content=content[1:]
 #make all the words lowercase
 content = [i.lower() for i in content]
-
 #---------------------Importing Json-----------
 
 with open("scoreboard.json","r") as f:
@@ -45,13 +44,16 @@ score=0
 errors=0
 time_left=0
 word_to_guess = list((random.choice(content)).lower()) #word to guess as a list
-word_to_guess_display = list('_'*len(word_to_guess)) #word to guess as a list
+word_to_guess_display = list('_ '*len(word_to_guess)) #word to guess as a list
 easy_scoreboard=""
 medium_scoreboard=""
 hard_scoreboard=""
+gamemode="Easy"
+game_over=False
 easy_scoreboard_data=scoreboard[([i for i in scoreboard])[0]]
 medium_scoreboard_data=scoreboard[([i for i in scoreboard])[1]]
 hard_scoreboard_data=scoreboard[([i for i in scoreboard])[2]]
+
 
 
 #---------------------Functions----------------------------
@@ -75,6 +77,60 @@ def scoreboardprint_sorted():
     for i in hard_scoreboard_data:
         hard_scoreboard+=i+" : "+str(hard_scoreboard_data[i])+" points"+"\n"
 
-def score_calculate(elapsed_time,tries_left):
-    return int(1000*(tries_left/elapsed_time))
+def score_calculate(elapsed_time,errors_left):
+    return int(1000*(errors_left/elapsed_time))
+
+def guess_word_to_guess_display(letter):
+    global word_to_guess
+    global word_to_guess_display
+    global score
+    global errors
+    if letter in word_to_guess:
+        for i in range(len(word_to_guess)):
+            if word_to_guess[i]==letter:
+                word_to_guess_display[i]=letter
+    else:
+        errors+=1
+
+def check_if_win():
+    global word_to_guess
+    global word_to_guess_display
+    if word_to_guess==word_to_guess_display:
+        return True
+    else:
+        return False
+
+def check_if_lose():
+    global errors
+    global MODE
+    global gamemode
+    if errors>=MODE[gamemode]["MAX_ERRORS"]:
+        return True
+    else:
+        return False
+
+def check_if_game_over():
+    global game_over
+    global time_left
+    if check_if_win() or check_if_lose() or time_left<=0:
+        game_over=True
+        return True
+    else:
+        return False
+
+def play_again():
+    global word_to_guess
+    global word_to_guess_display
+    global score
+    global errors
+    global time_left
+    global game_over
+    global gamemode
+    word_to_guess = list((random.choice(content)).lower()) #word to guess as a list
+    word_to_guess_display = list('_ '*len(word_to_guess)) #word to guess as a list
+    score=0
+    errors=0
+    time_left=0
+    game_over=False
+    gamemode=""#current gamemode
 
