ea634b918295f4e68475046cb9cee85972257f5d
Fensoa <hugo.andriamaromanana@laplateforme.io>
2023-01-23 12:16:00 +0100

    [ADD] title

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ RED=(255,0,0)
 GREEN=(0,255,0)
 BLUE=(0,0,255)
 YELLOW=(255,255,0)
+ORANGE=(255,165,0)
 MODE={
     "Easy":{
         "MAX_TIME":180,
@@ -106,7 +107,6 @@ def play_again():
     errors=0
     time_left=0
     game_over=False
-    gamemode=""#current gamemode
 
 def get_3_best(dic):
     arr=[]
@@ -128,24 +128,24 @@ screen.fill(WHITE)
 COMIC_SANS= pygame.font.SysFont('Comic Sans MS', 30)
 
 #Hard button
-hard_button_rect = pygame.Rect(650, 100, 120, 50)
+hard_button_rect = pygame.Rect(650, 100, 200, 50)
 hard_button_text = COMIC_SANS.render("Hard", True, BLACK)
 hard_button_rect_center=hard_button_text.get_rect(center=hard_button_rect.center)
 #Medium button
-medium_button_rect = pygame.Rect(650, 200, 120, 50)
+medium_button_rect = pygame.Rect(650, 200, 200, 50)
 medium_button_text = COMIC_SANS.render("Medium", True, BLACK)
 medium_button_rect_center=medium_button_text.get_rect(center=medium_button_rect.center)
 #Easy button
-easy_button_rect = pygame.Rect(650, 300, 120, 50)
+easy_button_rect = pygame.Rect(650, 300, 200, 50)
 easy_button_text = COMIC_SANS.render("Easy", True, BLACK)
 easy_button_rect_center=easy_button_text.get_rect(center=easy_button_rect.center)
 #Exit button
-exit_button_rect = pygame.Rect(650, 400, 120, 50)
+exit_button_rect = pygame.Rect(650, 500, 200, 50)
 exit_button_text = COMIC_SANS.render("Exit", True, WHITE)
 exit_button_rect_center=exit_button_text.get_rect(center=exit_button_rect.center)
 #Scoreboard button
-scoreboard_button_rect = pygame.Rect(300, 10, 160, 50)
-scoreboard_button_text = COMIC_SANS.render("Scoreboard", True, BLACK)
+scoreboard_button_rect = pygame.Rect(650, 400, 200, 50)
+scoreboard_button_text = COMIC_SANS.render("Scoreboard", True, WHITE)
 scoreboard_button_rect_center=scoreboard_button_text.get_rect(center=scoreboard_button_rect.center)
 #Scoreboard popup window
 scoreboard_popup=pygame.display.set_mode((1000, 650))
@@ -154,28 +154,37 @@ scoreboard_popup.fill(WHITE)
 scoreboard_popup_exit_button_rect = pygame.Rect(700,10, 50, 50)
 scoreboard_popup_exit_button_text = COMIC_SANS.render("X", True, WHITE)
 scoreboard_popup_exit_button_rect_center=scoreboard_popup_exit_button_text.get_rect(center=scoreboard_popup_exit_button_rect.center)
+#Title text
+# title_text_rect=pygame.Rect(0, 50, 800, 50)
+title_text = COMIC_SANS.render("Hangman: now on Windows XP!", True, BLACK)
+title_text_rect=title_text.get_rect(center=(400, 50))
+
+
             
 state="main_menu"
 
 def game_state(state):
     if state=="main_menu":
-        pygame.draw.rect(screen, RED, exit_button_rect)
+        pygame.draw.rect(screen, BLACK, exit_button_rect)
         screen.blit(exit_button_text, exit_button_rect_center)
-        pygame.draw.rect(screen, GREEN, hard_button_rect)
+        pygame.draw.rect(screen, RED, hard_button_rect)
         screen.blit(hard_button_text, hard_button_rect_center)
-        pygame.draw.rect(screen, YELLOW, medium_button_rect)
+        pygame.draw.rect(screen, ORANGE, medium_button_rect)
         screen.blit(medium_button_text, medium_button_rect_center)
-        pygame.draw.rect(screen, BLUE, easy_button_rect)
+        pygame.draw.rect(screen, YELLOW, easy_button_rect)
         screen.blit(easy_button_text, easy_button_rect_center)
-        pygame.draw.rect(screen, BLACK, scoreboard_button_rect)
+        pygame.draw.rect(screen, BLUE, scoreboard_button_rect)
         screen.blit(scoreboard_button_text, scoreboard_button_rect_center)
+        pygame.draw.rect(screen, GREEN, title_text_rect)
+        screen.blit(title_text, title_text_rect)
     elif state=="scoreboard":
         pygame.draw.rect(scoreboard_popup, RED, scoreboard_popup_exit_button_rect)
         scoreboard_popup.blit(scoreboard_popup_exit_button_text, scoreboard_popup_exit_button_rect_center)
-        scoreboard_popup.blit(COMIC_SANS.render("SCOREBOARD", True, YELLOW), (400, 10))
-        scoreboard_popup.blit(COMIC_SANS.render("Easy", True, GREEN), (100, 100))
-        scoreboard_popup.blit(COMIC_SANS.render("Medium", True, BLUE), (450, 100))
+        scoreboard_popup.blit(COMIC_SANS.render("SCOREBOARD", True, BLUE), (400, 10))
+        scoreboard_popup.blit(COMIC_SANS.render("Easy", True, YELLOW), (100, 100))
+        scoreboard_popup.blit(COMIC_SANS.render("Medium", True, ORANGE), (450, 100))
         scoreboard_popup.blit(COMIC_SANS.render("Hard", True, RED), (800, 100))
+        sort_scoreboard_data_by_score()
         scoreboard_popup.blit(COMIC_SANS.render("1st "+get_3_best(easy_scoreboard_data)[0], True, BLACK), (20, 200))
         scoreboard_popup.blit(COMIC_SANS.render("2nd "+get_3_best(easy_scoreboard_data)[1], True, BLACK), (20, 300))
         scoreboard_popup.blit(COMIC_SANS.render("3rd "+get_3_best(easy_scoreboard_data)[2], True, BLACK), (20, 400))
@@ -185,6 +194,8 @@ def game_state(state):
         scoreboard_popup.blit(COMIC_SANS.render("1st "+get_3_best(hard_scoreboard_data)[0], True, BLACK), (720, 200))
         scoreboard_popup.blit(COMIC_SANS.render("2nd "+get_3_best(hard_scoreboard_data)[1], True, BLACK), (720, 300))
         scoreboard_popup.blit(COMIC_SANS.render("3rd "+get_3_best(hard_scoreboard_data)[2], True, BLACK), (720, 400))
+    # elif state=="hard":
+
 
 
 while running:
